8882210f5248d1f17c4b7d0a990ebb39b73fa449
vinil91 <lopuru897>
2024-05-22 11:58:27 +0300

    yo1

--- src/App.jsx
+++ src/App.jsx
@@ -22,15 +22,16 @@ const App = () => {
   }, []);
 
   return (
-      <Routes>
-          <Route path="/" element={<Layout />}>
-              <Route index element={<Main />} />
-              <Route path="services" element={<Services />} />
-              <Route path="prices" element={<Prices />} />
-              <Route path="blog" element={<Blog />} />
-              <Route path="about" element={<About />}/>
-          </Route>
-      </Routes>
+      // <Routes>
+      //     <Route path="/" element={<Layout />}>
+      //         <Route index element={<Main />} />
+      //         <Route path="services" element={<Services />} />
+      //         <Route path="prices" element={<Prices />} />
+      //         <Route path="blog" element={<Blog />} />
+      //         <Route path="about" element={<About />}/>
+      //     </Route>
+      // </Routes>
+      <div>ssf</div>
   );
 };
 
